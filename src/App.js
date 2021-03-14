a235f347b354d02368f92a5659a3bed6039d75fa
tnsnow <ungnhan1403@gmail.com>
2021-03-14 17:41:23 +0700

    change file app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          do an tong hop
+          Đồ án tổng hợp.
         </a>
       </header>
     </div>
