2d0fd29076a86014e41403fd9baefd2a6a8d6c93
UngPhuNhan <ungnhan1403@gmail.com>
2021-04-11 18:40:40 +0700

    install slick

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import '../src/assets/scss/styles.scss';
 
 //Pages import
 import Home from "./pages/Home";
