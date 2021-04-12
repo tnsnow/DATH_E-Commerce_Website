5b13586a6af99f0b78b5bab4ff4efb4bfb92d091
UngPhuNhan <ungnhan1403@gmail.com>
2021-04-12 14:00:06 +0700

    build section-banner

--- src/Components/Header/CoverBanner.js
+++ src/Components/Header/CoverBanner.js
@@ -5,13 +5,35 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import bannerImg from "../../assets/images/background_image/landscape.jpg";
 
-
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "red" }}
+            onClick={onClick}
+        />
+    );
+}
+function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "red" }}
+            onClick={onClick}
+        />
+    );
+}
 
 function CoverBanner(props) {
     const settingSlick = {
         dots: true,
         infinite: true,
-        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
         slidesToShow: 1,
         slidesToScroll: 1
     }
@@ -21,7 +43,7 @@ function CoverBanner(props) {
                 <div className="row">
                     <div className="col-lg-8">
                         <div className="section-banner__item section-banner__left">
-                            <Slider {...settingSlick}>
+                            <Slider className="banner-left" {...settingSlick}>
                                 <div className="slider-item">
                                     <a href="/">
                                         <img src={bannerImg} className="img-fluid" alt="Banner 1" />
@@ -41,7 +63,20 @@ function CoverBanner(props) {
                         </div>
                     </div>
                     <div className="col-lg-4">
-                        <div className="section-banner__item section-banner__right"></div>
+                        <div className="section-banner__item section-banner__right">
+                            <div className="banner-right">
+                                <div className="banner-right__item banner-right__top">
+                                    <a href="/">
+                                        <img src={bannerImg} className="img-fluid" />
+                                    </a>
+                                </div>
+                                <div className="banner-right__item banner-right__bottom">
+                                    <a href="/">
+                                        <img src={bannerImg} className="img-fluid" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
