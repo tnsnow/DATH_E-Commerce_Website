e614c011be6f563f656c01cfd2999b3ad896d853
Kennn-dev <nhatong2015@gmail.com>
2021-04-15 12:00:47 +0700

    404 page ,items group, category cmpnts added

--- src/Components/Header/CoverBanner.js
+++ src/Components/Header/CoverBanner.js
@@ -1,52 +1,50 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+// import PropTypes from 'prop-types';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import bannerImg from "../../assets/images/background_image/landscape.jpg";
 
-
-
 function CoverBanner(props) {
-    const settingSlick = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    }
-    return (
-        <div id="banner" className="section-banner" >
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-8">
-                        <div className="section-banner__item section-banner__left">
-                            <Slider {...settingSlick}>
-                                <div className="slider-item">
-                                    <a href="/">
-                                        <img src={bannerImg} className="img-fluid" alt="Banner 1" />
-                                    </a>
-                                </div>
-                                <div className="slider-item">
-                                    <a href="/">
-                                        <img src={bannerImg} className="img-fluid" alt="Banner 1" />
-                                    </a>
-                                </div>
-                                <div className="slider-item">
-                                    <a href="/">
-                                        <img src={bannerImg} className="img-fluid" alt="Banner 1" />
-                                    </a>
-                                </div>
-                            </Slider>
-                        </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="section-banner__item section-banner__right"></div>
-                    </div>
+  const settingSlick = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <div id="banner" className="section-banner">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8">
+            <div className="section-banner__item section-banner__left">
+              <Slider {...settingSlick}>
+                <div className="slider-item">
+                  <a href="/">
+                    <img src={bannerImg} className="img-fluid" alt="Banner 1" />
+                  </a>
                 </div>
+                <div className="slider-item">
+                  <a href="/">
+                    <img src={bannerImg} className="img-fluid" alt="Banner 1" />
+                  </a>
+                </div>
+                <div className="slider-item">
+                  <a href="/">
+                    <img src={bannerImg} className="img-fluid" alt="Banner 1" />
+                  </a>
+                </div>
+              </Slider>
             </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="section-banner__item section-banner__right"></div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default CoverBanner;
--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -1,94 +1,101 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { Menu, Input, Space, Button, Popover } from 'antd';
-import { QuestionCircleOutlined, ShoppingCartOutlined, BellOutlined } from '@ant-design/icons';
+import React, { useState } from "react";
+// import PropTypes from 'prop-types';
+import {
+  Menu,
+  Input,
+  Space,
+  // Button,
+  // Popover
+} from "antd";
+import {
+  QuestionCircleOutlined,
+  ShoppingCartOutlined,
+  BellOutlined,
+} from "@ant-design/icons";
 import logo from "../../assets/images/logo/logo_cartya_black.png";
 
-const { SubMenu } = Menu;
+// const { SubMenu } = Menu;
 const { Search } = Input;
 
 function Navbar(props) {
-    const [current, setCurrent] = useState();
+  const onSearch = (value) => console.log(value);
 
-    const handleClick = (e) => {
-        console.log('click ', e);
-        setCurrent(e.key);
-    };
-
-    const onSearch = value => console.log(value);
-
-    return (
-        <div id="menuComponent" className="section-navbar" >
-            <div className="container">
-                <div className="d-flex justify-content-between section-navbar__top">
-                    <div className="section-navbar__item section-navbar__top--left">
-                        <Menu mode="horizontal">
-                            <Space>
-                                <Menu.Item>
-                                    <a href="#">Kênh người bán</a>
-                                </Menu.Item>
-                                <Menu.Item>
-                                    <div className="d-flex">
-                                        <p>Kết nối với chúng tôi:</p>
-                                        <div className="d-flex social">
-                                            <div className="social__item">
-                                                <a href="#">fb</a>
-                                            </div>
-                                            <div className="social__item">
-                                                <a href="#">ins</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Menu.Item>
-                            </Space>
-                        </Menu>
+  return (
+    <div id="menuComponent" className="section-navbar">
+      <div className="container">
+        <div className="d-flex justify-content-between section-navbar__top">
+          <div className="section-navbar__item section-navbar__top--left">
+            <Menu mode="horizontal">
+              <Space>
+                <Menu.Item>
+                  <a href="#">Kênh người bán</a>
+                </Menu.Item>
+                <Menu.Item>
+                  <div className="d-flex">
+                    <p>Kết nối với chúng tôi:</p>
+                    <div className="d-flex social">
+                      <div className="social__item">
+                        <a href="#">fb</a>
+                      </div>
+                      <div className="social__item">
+                        <a href="#">ins</a>
+                      </div>
                     </div>
-                    <div className="section-navbar__item section-navbar__top--right">
-                        <Menu mode="horizontal">
-                            <Space>
-                                <Menu.Item>
-                                    <BellOutlined />
-                                    <span>Thông báo</span>
-                                </Menu.Item>
-                                <Menu.Item>
-                                    <a href="/Login">
-                                        <QuestionCircleOutlined />
-                                        <span>Hỗ Trợ</span>
-                                    </a>
-                                </Menu.Item>
-                                <Menu.Item key="login">
-                                    <a href="/Login">Login</a>
-                                </Menu.Item>
-                                <Menu.Item key="register">
-                                    <a href="/Register">Register</a>
-                                </Menu.Item>
-                            </Space>
-                        </Menu>
-                    </div>
-                </div>
+                  </div>
+                </Menu.Item>
+              </Space>
+            </Menu>
+          </div>
+          <div className="section-navbar__item section-navbar__top--right">
+            <Menu mode="horizontal">
+              <Space>
+                <Menu.Item>
+                  <BellOutlined />
+                  <span>Thông báo</span>
+                </Menu.Item>
+                <Menu.Item>
+                  <a href="/Login">
+                    <QuestionCircleOutlined />
+                    <span>Hỗ Trợ</span>
+                  </a>
+                </Menu.Item>
+                <Menu.Item key="login">
+                  <a href="/Login">Login</a>
+                </Menu.Item>
+                <Menu.Item key="register">
+                  <a href="/Register">Register</a>
+                </Menu.Item>
+              </Space>
+            </Menu>
+          </div>
+        </div>
 
-                <div className="d-flex justify-content-between section-navbar__bottom">
-                    <Menu mode="horizontal">
-                        <Space>
-                            <Menu.Item>
-                                <a href="/">
-                                    <img src={logo} alt="Logo" />
-                                </a>
-                            </Menu.Item>
-                            <Menu.Item>
-                                <Search placeholder="input search text" onSearch={onSearch} enterButton />
-                            </Menu.Item>
-                            <Menu.Item>
-                                <a hrf="">
-                                    <ShoppingCartOutlined />
-                                </a>
-                            </Menu.Item>
-                        </Space>
-                    </Menu>
-                </div>
-            </div>
-        </div >
-    );
+        <div className="d-flex justify-content-between section-navbar__bottom">
+          <Menu mode="horizontal">
+            <Space>
+              <Menu.Item>
+                <a href="/">
+                  <img src={logo} alt="Logo" />
+                </a>
+              </Menu.Item>
+              <Menu.Item>
+                <Search
+                  placeholder="input search text"
+                  onSearch={onSearch}
+                  enterButton
+                />
+              </Menu.Item>
+              <Menu.Item>
+                <a hrf="">
+                  <ShoppingCartOutlined />
+                </a>
+              </Menu.Item>
+            </Space>
+          </Menu>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
