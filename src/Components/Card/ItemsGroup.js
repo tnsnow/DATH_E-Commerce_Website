6de36ff286534f29dfeb30f0179015d4b3bc60ea
Kennn-dev <nhatong2015@gmail.com>
2021-04-26 22:26:08 +0700

    fix fetch data

--- src/Components/Card/ItemsGroup.js
+++ src/Components/Card/ItemsGroup.js
@@ -4,35 +4,20 @@ import CardItem from "./CardItem";
 
 ItemsGroup.propTypes = {
   isLoading: PropTypes.bool,
-  data: PropTypes.object
+  data: PropTypes.object,
 };
 
 export default function ItemsGroup({ isLoading, data }) {
-
-  // const test = {
-  //   isLoading: false,
-  //   product: {
-  //     name: "Name product",
-  //     description:
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  //     rating: "4.5",
-  //     like: "30",
-  //     images: "https://picsum.photos/200",
-  //     price: "120000",
-  //     sold: "0",
-  //   },
-  // };
+  console.log(data);
   return (
-
     <div className="row">
-      {
-        data.data.data.map(product =>
-          <div className="col-lg-3 padding-items">
-            <CardItem isLoading={isLoading} product={product} />
-          </div>
-        )
-      }
+      {data
+        ? data.data.map((product) => (
+            <div className="col-lg-3 padding-items">
+              <CardItem isLoading={isLoading} product={product} />
+            </div>
+          ))
+        : ""}
     </div>
   );
 }
-
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Divider } from "antd";
 import { Switch, Route } from "react-router-dom";
-import { useQuery } from 'react-query'
-import axios from 'axios';
+import { useQuery } from "react-query";
+import axios from "axios";
 //components import here
 import CategoryTab from "../Components/Category/CategoryTab";
 import Navbar from "../Components/Header/Navbar";
@@ -15,17 +15,17 @@ import Trends from "../Components/Trends/Trends";
 import Promotions from "../Components/Promotions/Promotions";
 import ListSeller from "../Components/Sellers/ListSellers";
 
-
-
 export default function Home() {
   // const [data , setData] = useState([]);
   const fetchListCart = async () => {
+    return await axios.get("http://localhost:4001/products");
+  };
+  const { isLoading, isError, data, error } = useQuery(
+    "itemCart",
+    fetchListCart
+  );
 
-    return await axios.get("http://localhost:4001/products")
-  }
-  const { isLoading, isError, data, error } = useQuery('itemCart', fetchListCart);
-
-  if (isError) return <p>Error</p>
+  if (isError) return <p>{error}</p>;
 
   return (
     <>
@@ -44,10 +44,11 @@ export default function Home() {
               <Trends />
               <Promotions />
               <ListSeller />
-              {
-                isLoading ? <p>Loading ...</p> :
-                  <ItemsGroup isLoading={isLoading} data={data} />
-              }
+              {isLoading ? (
+                <p>Loading ...</p>
+              ) : (
+                <ItemsGroup isLoading={isLoading} data={data} />
+              )}
             </div>
           </Route>
           <Route path="/home/search/:keyword">
