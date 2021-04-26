c31e5518e340aff082c8b6f62f1e291dafbc5cde
Ừng Phú Nhân <nhanung.pix@gmail.com>
2021-04-26 22:05:29 +0700

    get api products section home page

--- src/Components/Card/ItemsGroup.js
+++ src/Components/Card/ItemsGroup.js
@@ -1,46 +1,38 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 import CardItem from "./CardItem";
 
-export default function ItemsGroup() {
-  const test = {
-    isLoading: false,
-    product: {
-      name: "Name product",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      rating: "4.5",
-      like: "30",
-      images: "https://picsum.photos/200",
-      price: "120000",
-      sold: "0",
-    },
-  };
+ItemsGroup.propTypes = {
+  isLoading: PropTypes.bool,
+  data: PropTypes.object
+};
+
+export default function ItemsGroup({ isLoading, data }) {
+
+  // const test = {
+  //   isLoading: false,
+  //   product: {
+  //     name: "Name product",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  //     rating: "4.5",
+  //     like: "30",
+  //     images: "https://picsum.photos/200",
+  //     price: "120000",
+  //     sold: "0",
+  //   },
+  // };
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-3 padding-items">
-          <CardItem isLoading={test.isLoading} product={test.product} />
-        </div>
-        <div className="col-lg-3 padding-items">
-          <CardItem isLoading={test.isLoading} product={test.product} />
-        </div>
-        <div className="col-lg-3 padding-items">
-          <CardItem isLoading={test.isLoading} product={test.product} />
-        </div>
-        <div className="col-lg-3 padding-items">
-          <CardItem isLoading={test.isLoading} product={test.product} />
-        </div>
-        <div className="col-lg-3 padding-items">
-          <CardItem isLoading={test.isLoading} product={test.product} />
-        </div>
-        <div className="col-lg-3 padding-items">
-          <CardItem isLoading={test.isLoading} product={test.product} />
-        </div>
-        <div className="col-lg-3 padding-items">
-          <CardItem isLoading={test.isLoading} product={test.product} />
-        </div>
-      </div>
+
+    <div className="row">
+      {
+        data.data.data.map(product =>
+          <div className="col-lg-3 padding-items">
+            <CardItem isLoading={isLoading} product={product} />
+          </div>
+        )
+      }
     </div>
   );
 }
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Divider } from "antd";
 import { Switch, Route } from "react-router-dom";
+import { useQuery } from 'react-query'
+import axios from 'axios';
 //components import here
 import CategoryTab from "../Components/Category/CategoryTab";
 import Navbar from "../Components/Header/Navbar";
@@ -13,12 +15,23 @@ import Trends from "../Components/Trends/Trends";
 import Promotions from "../Components/Promotions/Promotions";
 import ListSeller from "../Components/Sellers/ListSellers";
 
+
+
 export default function Home() {
+  // const [data , setData] = useState([]);
+  const fetchListCart = async () => {
+
+    return await axios.get("http://localhost:4001/products")
+  }
+  const { isLoading, isError, data, error } = useQuery('itemCart', fetchListCart);
+
+  if (isError) return <p>Error</p>
+
   return (
     <>
       <div className="box-nav">
         <div className="container">
-          <Navbar z />
+          <Navbar />
         </div>
       </div>
 
@@ -31,6 +44,10 @@ export default function Home() {
               <Trends />
               <Promotions />
               <ListSeller />
+              {
+                isLoading ? <p>Loading ...</p> :
+                  <ItemsGroup isLoading={isLoading} data={data} />
+              }
             </div>
           </Route>
           <Route path="/home/search/:keyword">
