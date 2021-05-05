e21be796d8916ae49f87fdb36bf34ea9866efe07
Kennn-dev <nhatong2015@gmail.com>
2021-05-05 21:25:05 +0700

    update develop

--- src/Components/Seller/products/ProductAdd.js
+++ src/Components/Seller/products/ProductAdd.js
@@ -1,5 +1,42 @@
+import { Form, Input, Button, Divider } from "antd";
 import React from "react";
 
 export default function ProductAdd() {
-  return <div>Addd</div>;
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 8,
+    },
+  };
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  return (
+    <div className="common-content">
+      <Divider orientation="left">Products Infomation</Divider>
+      <Form {...layout} name="basic" onFinish={onFinish}>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
