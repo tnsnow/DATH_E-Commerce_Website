27ee4921b99b15b9a5d7f72dbcbe8108a2436bb3
Duy Pham <duypham98.pix@gmail.com>
2021-06-17 18:11:32 +0700

    fix

--- src/Components/Detail/Product.js
+++ src/Components/Detail/Product.js
@@ -2,15 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Row, Col, Space } from "antd";
 
-<<<<<<< HEAD
 import ProductImage from "./ProductImage";
 import ProductDesc from "./ProductDesc";
 import Shop from "../Shop/Shop";
 import ProductInfo from "./ProductInfo";
-=======
-import ProductLeft from "./ProductLeft";
-import ProductRight from "./ProductRight";
->>>>>>> 84ac07fb5aaf582e501968eb1c7729016e2a5756
 
 Product.propTypes = {
   data: PropTypes.object,
@@ -20,7 +15,6 @@ Product.propTypes = {
 function Product({ data, isLoading }) {
   if (isLoading) return "";
   return (
-<<<<<<< HEAD
     <div className="section-product-detail">
       <Space size="large" direction="vertical" style={{ width: "100%" }}>
         <Row gutter={24}>
@@ -38,17 +32,6 @@ function Product({ data, isLoading }) {
           </Space>
         </Row>{" "}
       </Space>
-=======
-    <div className="section-product-detail__content">
-      <Row gutter={24}>
-        <Col span="10">
-          <ProductLeft images={images} data={data} />
-        </Col>
-        <Col span="14">
-          <ProductRight data={data} />
-        </Col>
-      </Row>
->>>>>>> 84ac07fb5aaf582e501968eb1c7729016e2a5756
     </div>
   );
 }
